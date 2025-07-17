7271229da633e7fc968d9e4e354aa065de68bc08
uistn <d2mcvica@uwaterloo.ca>
2025-07-16 22:43:02 -0400

    added arduino example of picking up and dropping lego piece

---
+++ Arduino/498_Motor_Test/picking_up_piece_example.ino
@@ -0,0 +1,44 @@
+const int IN1 = 10;
+const int IN2 = 11;
+const int PUMP = 9;
+
+void setup() {
+  // Set motor control pins as outputs
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(PUMP, OUTPUT);
+}
+
+void loop() {
+  delay(5000);
+  // Reverse rotation GO DOWN
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  delay(1800);  // run for 1.8 seconds
+
+  // Brake
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  delay(2000);
+
+  // Turn pump on
+  digitalWrite(PUMP, HIGH);
+
+  delay(200);
+
+  // Forward rotation GO UP
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  delay(2100);
+
+  // Brake
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  delay(2000);
+
+  // Turn Pump off
+  digitalWrite(PUMP, LOW);
+
+  delay(4000);
+
+}
